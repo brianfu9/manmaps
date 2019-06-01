5411d4a4b66b06efe951bb7500ffdca7515a1664
brian fu <brianfu9@gmail.com>
2019-06-01 14:05:51 -0700

    one page layout

--- app/src/main/java/com/example/manmaps/MainActivity.java
+++ app/src/main/java/com/example/manmaps/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.manmaps;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -15,7 +18,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -24,20 +30,32 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    public static final String DESTINATION_LATITUDE = "com.example.myfirstapp.MESSAGE1";
-    public static final String DESTINATION_LONGITUDE = "com.example.myfirstapp.MESSAGE2";
     private static final String TAG = MainActivity.class.getName();
-    Geocoder geocoder;
-    private FusedLocationProviderClient fusedLocationClient;
-    private LocationCallback locationCallback;
-    private Location myLocation;
+    ImageView compass_img;
+    TextView txt_compass;
+    TextView txt_location;
     LocationRequest locationRequest;
-
+    int mAzimuth;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private Geocoder geocoder;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location myLocation;
+    private Location myDestination;
+    private LocationCallback locationCallback;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         geocoder = new Geocoder(this, Locale.getDefault());
 
+        myDestination = null;
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         locationRequest = LocationRequest.create();
@@ -60,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 for (Location location : locationResult.getLocations()) {
                     myLocation = location;
                 }
-                System.out.println("********************    myLocation " + myLocation.getLongitude() + ", " + myLocation.getLatitude());
+                System.out.println("myLocation " + myLocation.getLongitude() + ", " + myLocation.getLatitude());
             }
         };
 
@@ -69,7 +88,13 @@ public class MainActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 42);
         }
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        compass_img = findViewById(R.id.img_arrow);
+        txt_compass = findViewById(R.id.txt_bearing);
+        txt_location = findViewById(R.id.txt_location);
+
+        start();
     }
 
     @Override
@@ -84,14 +109,35 @@ public class MainActivity extends AppCompatActivity {
         start();
     }
 
-    // *****************
+//    ********************
 
     public void start() {
         startLocationUpdates();
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();
+            } else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        } else {
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
     }
 
     public void stop() {
         stopLocationUpdates();
+
+        if (haveSensor) {
+            mSensorManager.unregisterListener(this, mRotationV);
+        } else {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        }
     }
 
     private void startLocationUpdates() {
@@ -106,14 +152,67 @@ public class MainActivity extends AppCompatActivity {
         fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 
+//    ********************
 
-    /**
-     * Called when the user taps the Send button
-     */
-    public void sendMessage(View view) {
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        try {
+            mAzimuth = ((mAzimuth - Math.round(myLocation.bearingTo(myDestination))) + 360) % 360;
+            compass_img.setRotation(-mAzimuth);
+            txt_compass.setText(mAzimuth + "°");
+            txt_location.setText(NumberFormat.getNumberInstance(Locale.US).format(Math.round(myLocation.distanceTo(myDestination))) + " m");
+            System.out.println(mAzimuth);
+        } catch (Exception e) {
+            System.out.println(e);
+            txt_location.setText("I'm lost");
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    public void noSensorsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        Intent intent = new Intent(this, CompassActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+//    ********************
+
+    public void sendMessage(View view) {
+
+        hideKeyboard(this);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
 
         List<Address> addresses = null;
@@ -125,11 +224,19 @@ public class MainActivity extends AppCompatActivity {
             // Catch network or other I/O problems.
             errorMessage = "service_not_available";
             Log.e(TAG, errorMessage, ioException);
+            alertDialog.setMessage(errorMessage)
+                    .setCancelable(true)
+                    .setNegativeButton("Close", null);
+            alertDialog.show();
         } catch (IllegalArgumentException illegalArgumentException) {
             // Catch invalid latitude or longitude values.
-            errorMessage = "invalid_lat_long_used";
+            errorMessage = "invalid_address_identifier";
             Log.e(TAG, errorMessage + ". " +
                     "Querry string = " + message, illegalArgumentException);
+            alertDialog.setMessage(errorMessage)
+                    .setCancelable(true)
+                    .setNegativeButton("Close", null);
+            alertDialog.show();
         }
 
         // Handle case where no address was found.
@@ -137,17 +244,13 @@ public class MainActivity extends AppCompatActivity {
             if (errorMessage.isEmpty()) {
                 errorMessage = "no_address_found";
                 Log.e(TAG, errorMessage);
+                alertDialog.setMessage(errorMessage)
+                        .setCancelable(true)
+                        .setNegativeButton("Close", null);
+                alertDialog.show();
             }
-            alertDialog.setMessage("Location not found")
-                    .setCancelable(false)
-                    .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    });
-            alertDialog.show();
-            System.out.println("\n\nBORKEN " + errorMessage + "\n\n");
         } else {
-            Address address = null;
+            Address address = addresses.get(0);
             double best = Double.MAX_VALUE;
             for (Address a : addresses) {
                 Location dummy = new Location("dummy");
@@ -162,14 +265,26 @@ public class MainActivity extends AppCompatActivity {
 
             Log.i(TAG, "address_found");
 
-            intent.putExtra(DESTINATION_LATITUDE, address.getLatitude() + "");
-            intent.putExtra(DESTINATION_LONGITUDE, address.getLongitude() + "");
-            System.out.println("******************** WORKEN " + address.getLatitude() + " " + address.getLongitude());
-            startActivity(intent);
+            myDestination = new Location("Brian");
+            myDestination.setLatitude(address.getLatitude());
+            myDestination.setLongitude(address.getLongitude());
+
+            System.out.println("Address geocoords: " + address.getLatitude() + " " + address.getLongitude());
         }
     }
 
-    //**************************
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+//    ********************
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
